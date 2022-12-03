d57b353a768c0545b3f2372e68da08b63e3279e9
AlphaKeks <alphakeks@dawn.sh>
2022-12-03 17:07:55 +0100

    better template

--- .template/src/main.rs
+++ .template/src/main.rs
@@ -1,21 +1,57 @@
 #![allow(dead_code)]
 
-fn main() -> anyhow::Result<()> {
-	const INPUT: &str = include_str!("../input.txt");
-
-	return Ok(());
-}
-
 struct PartA;
 impl PartA {
+	#[allow(unused_variables)]
 	pub fn solve(input: &str) -> usize {
-		return 0;
+		return 69;
 	}
 }
 
 struct PartB;
 impl PartB {
+	#[allow(unused_variables)]
 	pub fn solve(input: &str) -> usize {
-		return 0;
+		return 69;
+	}
+}
+
+#[allow(unused_variables)]
+fn main() -> anyhow::Result<()> {
+	let input = std::fs::read_to_string("input.txt")?;
+
+	let result_a = PartA::solve(&input);
+	println!("PartA: {:?}", result_a);
+
+	let result_b = PartB::solve(&input);
+	println!("PartB: {:?}", result_b);
+
+	return Ok(());
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	#[ignore]
+	fn part_a() -> anyhow::Result<()> {
+		let input = std::fs::read_to_string("test_input.txt")?;
+		let result = PartA::solve(&input);
+
+		assert_eq!(69, result);
+
+		return Ok(());
+	}
+
+	#[test]
+	#[ignore]
+	fn part_b() -> anyhow::Result<()> {
+		let input = std::fs::read_to_string("test_input.txt")?;
+		let result = PartB::solve(&input);
+
+		assert_eq!(69, result);
+
+		return Ok(());
 	}
 }
